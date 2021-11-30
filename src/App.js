2bef0b9cc3e847de22fa834f2fa36c7b9346c5d9
Ayyub Iqbal <abuayubiqbal@gmail.com>
2021-11-30 23:55:34 +0600

    sidebar added with navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <div className="sidebar"></div>
+      <div className="sidebar">
+        <NavBar />
+      </div>
       <div className="main-content"></div>
     </div>
   );
---
+++ src/Components/NavBar.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import avatar from '../img/iqbal2.png';
+
+const NavBar = () => {
+    return (
+        <div>
+            <div className="NavBar">
+                <nav className="nav">
+                    <div className="profile">
+                        <img src={avatar} alt="" />
+                    </div>
+                    <ul className="nav-items">
+                        <li className="nav-item">
+                            <NavLink to="/" exact activeClassName="active">
+                                Home
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/about" exact activeClassName="active">
+                                About
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/portfolios" exact activeClassName="active">
+                                Portfolios
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/blogs" exact activeClassName="active">
+                                Blogs
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink to="/contact" exact activeClassName="active">
+                                Contact
+                            </NavLink>
+                        </li>
+                    </ul>
+                    <footer className="footer">
+                        <p> &copy; Copyright 2022. All Rights Reserved.</p>
+                    </footer>
+                </nav>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
